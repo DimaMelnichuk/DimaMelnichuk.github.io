dfbd4d3019a5f2a6ec2e4e191c34c4a8d8a0a76d
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-06-10 09:32:00 +0300

    Add new site for works

---
+++ js/dynamic_adapt.js
@@ -0,0 +1,27 @@
+//Объявляем переменные
+const parent_original = document.querySelector('.social-media__list');
+const parent = document.querySelector('.footer');
+const item = document.querySelector('.content__block_item');
+
+//Слушаем изменение размера экрана
+window.addEventListener('resize', move);
+
+//Функция
+function move(){
+	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	if (viewport_width <= 770) {
+		if (!item.classList.contains('done')) {
+			parent.insertBefore(item, parent.children[0]);
+			item.classList.add('done');
+		}
+	} else {
+		if (item.classList.contains('done')) {
+			parent_original.insertBefore(item, parent_original.children[0]);
+			item.classList.remove('done');
+		}
+	}
+}
+
+//Вызываем функцию
+move();
+

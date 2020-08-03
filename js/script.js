527b22f063d28b1b14301e8a27a9d5b8a2221118
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-08-04 01:30:31 +0300

    Update portfolio

--- js/script.js
+++ js/script.js
@@ -1,72 +1,35 @@
-/***************** Header-fixed ********************/
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-// Get the header
-var header = document.querySelector(".header");
-var headerContainer = document.querySelector(".header__container");
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
+	const body = document.querySelector('body');
+	const toggle = document.getElementById('toggle');
+	const input = document.getElementById('switch');
 
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("fixed");
-    headerContainer.classList.add("fixed");
-  } else {
-    header.classList.remove("fixed");
-    headerContainer.classList.remove("fixed");
-  }
-}
-
-/***************** Menu-btn ********************/
-let menu_burger = document.querySelector('.menu__burger');
-let menu = document.querySelector('.menu');
-let menu_lock = document.querySelector('body');
-menu_burger.addEventListener("click", function (e) {
-	menu.classList.toggle('active');
-	menu_burger.classList.toggle('active');
-	menu_lock.classList.toggle('lock');
-});
-
-
-/***************** WebP ********************/
-function testWebP(callback) {
-	var webP = new Image();
-	webP.onload = webP.onerror = function () {
-		callback(webP.height == 2);
-	};
-	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-testWebP(function (support) {
-	if (support == true) {
-		document.querySelector('body').classList.add('webp');
-	}
-});
-
-//Объявляем переменные
-const parent_original = document.querySelector('.social-media__list');
-const parent = document.querySelector('.footer');
-const item = document.querySelector('.content__block_item');
-
-//Слушаем изменение размера экрана
-window.addEventListener('resize', move);
-
-//Функция
-function move(){
-	const viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	if (viewport_width <= 830) {
-		if (!item.classList.contains('done')) {
-			parent.insertBefore(item, parent.children[0]);
-			item.classList.add('done');
+	toggle.addEventListener('click', function() {
+		const isChecked = input.checked;
+		if (isChecked) {
+			body.classList.remove('night');
+			toggle.classList.remove('active');
+		} else {
+			body.classList.add('night');
+			toggle.classList.add('active');
 		}
-	} else {
-		if (item.classList.contains('done')) {
-			parent_original.insertBefore(item, parent_original.children[0]);
-			item.classList.remove('done');
+	});
+
+	const scrollUp = document.querySelector('.scroll-up');
+	const offset = 800;
+	const getTop = () => window.pageYOffset || document.documentElement.scrollTop;
+
+	// onScroll
+	window.addEventListener('scroll', () => {
+		if (getTop() > offset) {
+			scrollUp.classList.add('scroll-up--active');
+		} else {
+			scrollUp.classList.remove('scroll-up--active');
 		}
-	}
-}
+	});
 
-//Вызываем функцию
-move();
-
+	//click
+	scrollUp.addEventListener('click', () => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth"
+		});
+	});
